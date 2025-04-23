78f6fa75c69e4f6c35213f0fd39f6faecb3b06d4
Manoj Pudasaini <mpudasaini17@gmail.com>
2025-04-23 16:56:10 +0545

    feat: implement streaming, vercel ai

---
+++ sst.config.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "sst-nextjs",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+      providers: {
+        aws: {
+          profile: "manojpudasaini"
+        }
+      },
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs("MyWeb", {
+      environment: {
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY!,
+      },
+    });
+  },
+});
